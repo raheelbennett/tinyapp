0a04c2a31c60197dc36440f8f5d1a8fd1449e4e3
Raheel Bennett <raheel.bennett@telus.com>
2022-11-28 16:44:09 -0700

    ran eslint

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
- res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
