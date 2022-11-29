b5c5e902e65e1879d90964e714aebb2cbf0c7cc1
Raheel Bennett <raheel.bennett@telus.com>
2022-11-28 19:09:38 -0700

    filled out urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -19,8 +19,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  // When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
