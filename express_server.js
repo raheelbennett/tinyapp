79f5cdfb004f84e32960a1ceb8c966b2bcd7917b
Raheel Bennett <raheel.bennett@telus.com>
2022-11-30 18:38:55 -0700

    Create a new template with a form containing email and password field and Form should POST to /register but endpoint not created yet and Add a GET route for /register which renders the registration template

--- express_server.js
+++ express_server.js
@@ -94,6 +94,11 @@ app.post("/logout", (req, res) => {
 });
 
 
+//Registration page
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
